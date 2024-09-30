a7568823937fbfb203d9731905adaad6ab5684b9
odd:odd <eehd80yul@gmail.com>
2024-09-30 13:48:23 +0900

    layout 변경

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,6 @@
 // app/layout.tsx
+'use client';
+
 import '@/styles/globals.scss';
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/Footer";
@@ -7,11 +9,17 @@ import {LayoutProps} from "@/types/layout";
 export default function RootLayout({ children }: LayoutProps) {
     return (
         <html lang="en">
-        <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        </body>
+            <body>
+                <div
+                    id="wrap"
+                >
+                     <Header />
+                    <div id="container">
+                        <div className="inner">{children}</div>
+                    </div>
+                    <Footer/>
+                </div>
+            </body>
         </html>
     );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
 const Page = () => {
     return (
-        <div>
+        <>
         <h1>Home Page</h1>
-        </div>
+        </>
     );
 }
 
